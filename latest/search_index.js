0dd3ad59e3f805c2a1be1c538a8a6164e0014948
autodocs <autodocs>
2017-08-19 16:59:20 +0000

    build based on 73dbfd8

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#JuliennedArrays.jl-1",
+    "page": "Home",
+    "title": "JuliennedArrays.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Demo-1",
+    "page": "Home",
+    "title": "Demo",
+    "category": "section",
+    "text": "This package contains a fast iterator for slicing (julienning) arrays.\n## Index\n@index@autodocs Modules = [JuliennedArrays] ```"
+},
+
+]}
