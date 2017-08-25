6059b933bbcdd6ac36fd0fa8c660dd0dea867563
autodocs <autodocs>
2017-08-25 06:05:28 +0000

    build based on 771bda0

--- latest/search_index.js
+++ latest/search_index.js
@@ -25,11 +25,43 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#JuliennedArrays.combine-Tuple{Base.Generator{T,F} where F where T<:JuliennedArrays.ReiteratedArray}",
+    "location": "index.html#JuliennedArrays.OutOfPlaceArray",
+    "page": "Home",
+    "title": "JuliennedArrays.OutOfPlaceArray",
+    "category": "Type",
+    "text": "struct OutOfPlaceArray{F} <: FunctionOptimization; f::F; end\n\nA function with a faster in-place analog on a whole preallocated output.\n\n\n\n"
+},
+
+{
+    "location": "index.html#JuliennedArrays.Reduction",
+    "page": "Home",
+    "title": "JuliennedArrays.Reduction",
+    "category": "Type",
+    "text": "struct Reduction{F} <: FunctionOptimization; f::F; end\n\nA reduction of another function\n\n\n\n"
+},
+
+{
+    "location": "index.html#JuliennedArrays.Swap",
+    "page": "Home",
+    "title": "JuliennedArrays.Swap",
+    "category": "Type",
+    "text": "struct Swap{F} <: FunctionOptimization; f::F; end\n\nA function that can be reused on a temporary array\n\n\n\n"
+},
+
+{
+    "location": "index.html#JuliennedArrays.View",
+    "page": "Home",
+    "title": "JuliennedArrays.View",
+    "category": "Type",
+    "text": "struct View{F} <: FunctionOptimization; f::F; end\n\nA function that should be called on views, not copies\n\n\n\n"
+},
+
+{
+    "location": "index.html#JuliennedArrays.combine-Tuple{Base.Generator{#s2,F} where F where #s2<:JuliennedArrays.ReindexedArray}",
     "page": "Home",
     "title": "JuliennedArrays.combine",
     "category": "Method",
-    "text": "combine(pieces)\n\nCombine many pieces of an array.\n\njulia> using JuliennedArrays\n\njulia> array = reshape([1 3 2; 5 6 4; 7 9 8], 3, 3)\n3×3 Array{Int64,2}:\n 1  3  2\n 5  6  4\n 7  9  8\n\njulia> combine(Base.Generator(sort, julienne(array, (*, :))))\n3×3 Array{Int64,2}:\n 1  2  3\n 4  5  6\n 7  8  9\n\n\n\n"
+    "text": "combine(pieces)\n\nCombine many pieces of an array.\n\njulia> using JuliennedArrays\n\njulia> array = [1 3 2; 5 6 4; 7 9 8]\n3×3 Array{Int64,2}:\n 1  3  2\n 5  6  4\n 7  9  8\n\njulia> result = combine(Base.Generator(sort, julienne(array, (*, :))))\n3×3 Array{Int64,2}:\n 1  2  3\n 4  5  6\n 7  8  9\n\njulia> array == result\nfalse\n\njulia> result2 = combine(Base.Generator(View(sort!), julienne(array, (*, :))))\n3×3 Array{Int64,2}:\n 1  2  3\n 4  5  6\n 7  8  9\n\njulia> array == result2\ntrue\n\n\n\n"
 },
 
 {
@@ -41,11 +73,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Base.map-Tuple{Any,JuliennedArrays.ReiteratedArray}",
+    "location": "index.html#Base.map-Tuple{Any,JuliennedArrays.ReindexedArray}",
     "page": "Home",
     "title": "Base.map",
     "category": "Method",
-    "text": "Base.map(r::ReiteratedArray)\n\njulia> using JuliennedArrays\n\njulia> array = reshape(1:9, 3, 3)\n3×3 Base.ReshapedArray{Int64,2,UnitRange{Int64},Tuple{}}:\n 1  4  7\n 2  5  8\n 3  6  9\n\njulia> map(sum, julienne(array, (:, *)))\n1×3 Array{Int64,2}:\n 6  15  24\n\njulia> map(median, julienne(array, (:, *)))\n1×3 Array{Float64,2}:\n 2.0  5.0  8.0\n\njulia> map(mean, julienne(array, (:, *)))\n1×3 Array{Float64,2}:\n 2.0  5.0  8.0\n\n\n\n"
+    "text": "Base.map(f, r::ReindexedArray)\n\njulia> using JuliennedArrays\n\njulia> array = reshape(1:9, 3, 3)\n3×3 Base.ReshapedArray{Int64,2,UnitRange{Int64},Tuple{}}:\n 1  4  7\n 2  5  8\n 3  6  9\n\njulia> map(sum, julienne(array, (:, *)))\n1×3 Array{Int64,2}:\n 6  15  24\n\njulia> map(median, julienne(array, (:, *)))\n1×3 Array{Float64,2}:\n 2.0  5.0  8.0\n\njulia> map(mean, julienne(array, (:, *)))\n1×3 Array{Float64,2}:\n 2.0  5.0  8.0\n\n\n\n"
 },
 
 {
