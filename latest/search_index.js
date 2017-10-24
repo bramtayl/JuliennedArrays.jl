459c8f2925c2a4ec9f16cad2f9c033f2d039485e
autodocs <autodocs>
2017-10-24 00:07:41 +0000

    build based on 2b14809

--- latest/search_index.js
+++ latest/search_index.js
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.Reduction",
     "category": "Type",
-    "text": "struct Reduction{F} <: FunctionOptimization; f::F; end\n\nA reduction of another function. Enables optimizations in some cases. Automatically enabled for known reduction functions.\n\njulia> using JuliennedArrays, Base.Test, Base.Test\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Swaps, array, (*, :));\n\njulia> @inferred map(Reduction(+), swaps)\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\njulia> @inferred map(sum, swaps)\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\n\n\n"
+    "text": "struct Reduction{F}\n\nA reduction of another function. Enables optimizations in some cases.\n\njulia> using JuliennedArrays, Base.Test, Base.Test\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Views, array, (*, :));\n\njulia> @inferred map(Reduction(+), swaps)\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.align",
     "category": "Method",
-    "text": "align(slices, code)\n\nAlign an array of slices into a larger array. Code should be a tuple for each dimension of the desired output. Slices will slide into dimensions coded by :, while * indicates dimensions taken up by the container array. Each slice should be EXACTLY the same size.\n\njulia> using JuliennedArrays, MappedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Swaps, array, code);\n\njulia> @inferred align(mappedarray(sort, swaps), code)\n3×3 Array{Int64,2}:\n 4  5  6\n 1  2  3\n 7  8  9\n\n\n\n"
+    "text": "align(slices, code)\n\nAlign an array of slices into a larger array. Code should be a tuple with an entry for each dimension of the desired output. Slices will slide into dimensions coded by :, while * indicates dimensions taken up by the container array. Each slice should be EXACTLY the same size.\n\njulia> using JuliennedArrays, MappedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Views, array, code);\n\njulia> @inferred align(mappedarray(sort, swaps), code)\n3×3 Array{Int64,2}:\n 4  5  6\n 1  2  3\n 7  8  9\n\n\n\n"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.julienne",
     "category": "Method",
-    "text": "julienne(T, array, code)\njulienne(T, array, code, swap)\n\nSlice an array and create shares of type T. T should be one of Arrays, Swaps, or Views. The code should a tuple of length ndims(array), where : indicates an axis parallel to slices and * indices an axis perpendicular to slices.\n\njulia> using JuliennedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> arrays = @inferred julienne(Arrays, array, (*, :));\n\njulia> @inferred map(mean, arrays)\n3-element Array{Float64,1}:\n 5.0\n 2.0\n 8.0\n\njulia> views = @inferred julienne(Views, array, (*, :));\n\njulia> @inferred map(mean, views)\n3-element Array{Float64,1}:\n 5.0\n 2.0\n 8.0\n\njulia> swaps = @inferred julienne(Swaps, array, (*, :));\n\njulia> @inferred map(mean, swaps)\n3-element Array{Float64,1}:\n 5.0\n 2.0\n 8.0\n\n\n\n"
+    "text": "julienne(T, array, code)\n\nSlice an array and create Parts of type T. T should be one of Arrays, Swaps, or Views. See the Parts package for more information. The code should a tuple of length ndims(array), where : indicates an axis parallel to slices and * indices an axis perpendicular to slices.\n\njulia> using JuliennedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> arrays = @inferred julienne(Views, array, (*, :));\n\njulia> @inferred map(sum, arrays)\n3-element Array{Int64,1}:\n 15\n  6\n 24\n\n\n\n"
 },
 
 {
