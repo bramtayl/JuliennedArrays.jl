9d67918aefa1c2a17748bbcb972a1d1e4d0f2112
autodocs <autodocs>
2018-01-10 17:30:52 +0000

    build based on 840cb92

--- latest/search_index.js
+++ latest/search_index.js
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.Reduction",
     "category": "Type",
-    "text": "struct Reduction{F}\n\nA reduction of another function. Enables optimizations in some cases.\n\njulia> using JuliennedArrays, Base.Test, Base.Test\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Views, array, (*, :));\n\njulia> @inferred map(Reduction(+), swaps)\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\n\n\n"
+    "text": "struct Reduction{F}\n\nA reduction of another function. Enables optimizations in some cases.\n\njulia> using JuliennedArrays, Base.Test, Base.Test\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> @inferred map(Reduction(+), julienne(array, (*, :)))\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.align",
     "category": "Method",
-    "text": "align(slices, code)\n\nAlign an array of slices into a larger array. Code should be a tuple with an entry for each dimension of the desired output. Slices will slide into dimensions coded by :, while * indicates dimensions taken up by the container array. Each slice should be EXACTLY the same size.\n\njulia> using JuliennedArrays, MappedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> swaps = @inferred julienne(Views, array, code);\n\njulia> @inferred align(mappedarray(sort, swaps), code)\n3×3 Array{Int64,2}:\n 4  5  6\n 1  2  3\n 7  8  9\n\n\n\n"
+    "text": "align(slices, code)\n\nAlign an array of slices into a larger array. Code should be a tuple with an entry for each dimension of the desired output. Slices will slide into dimensions coded by :, while * indicates dimensions taken up by the container array. Each slice should be EXACTLY the same size.\n\njulia> using JuliennedArrays, MappedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> views = @inferred julienne(array, code);\n\njulia> @inferred align(mappedarray(sort, views), code)\n3×3 Array{Int64,2}:\n 4  5  6\n 1  2  3\n 7  8  9\n\n\n\n"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "JuliennedArrays.julienne",
     "category": "Method",
-    "text": "julienne(T, array, code)\n\nSlice an array and create Parts of type T. T should be one of Arrays, Swaps, or Views. See the Parts package for more information. The code should a tuple of length ndims(array), where : indicates an axis parallel to slices and * indices an axis perpendicular to slices.\n\njulia> using JuliennedArrays, Base.Test\n\njulia> code = (*, :);\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> arrays = @inferred julienne(Views, array, (*, :));\n\njulia> @inferred map(sum, arrays)\n3-element Array{Int64,1}:\n 15\n  6\n 24\n\n\n\n"
+    "text": "julienne([T = Views], array, code)\n\nSlice an array and create Parts of type T. T should be one of Arrays, Swaps, or Views. It defaults to Views. See the Parts package for more information. The code should a tuple of length ndims(array), where : indicates an axis parallel to slices and * indices an axis perpendicular to slices.\n\njulia> using JuliennedArrays, Base.Test\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> @inferred map(sum, julienne(array, (*, :)))\n3-element Array{Int64,1}:\n 15\n  6\n 24\n\n\n\n"
 },
 
 {
