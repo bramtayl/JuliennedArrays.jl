1660c73bb2927b98584793bdabbbc78d923759f2
bramtayl <brandon.taylor221@gmail.com>
2018-08-16 16:55:00 -0400

    updates for 0.1

--- src/JuliennedArrays.jl
+++ src/JuliennedArrays.jl
@@ -1,29 +1,56 @@
 module JuliennedArrays
 
-using RecurUnroll: getindex_unrolled, setindex_many_unrolled, find_unrolled
-using TypedBools: True, False, not
-using Parts: AbstractParts, Arrays, Views, Swaps
+import Base: length, axes, setindex!, getindex, @propagate_inbounds, map
+using Keys: getindex_unrolled, setindex_unrolled, find_unrolled, True, False,
+    not, fill_tuple
 
-export Arrays
-export Views
-export Swaps
+maybe_tuple(t::Tuple) = t
+maybe_tuple(any) = (any,)
 
-import Base: indices, size, getindex, setindex!, @propagate_inbounds, map
+untuple(t) = t
+untuple(t::Tuple{A} where A) = first(t)
+
+struct Views{T, N, A, I} <: AbstractArray{T, N}
+    array::A
+    locations::I
+end
+
+Views(array::AbstractArray, locations::AbstractArray) =
+    Views{
+        typeof(@view array[maybe_tuple(first(locations))...]),
+        ndims(locations),
+        typeof(array),
+        typeof(locations)
+    }(array, locations)
+
+axes(v::Views) = axes(v.locations)
+length(v::Views) = length(v.locations)
+
+@propagate_inbounds getindex(views::Views{T, N}, index::Vararg{Int, N}) where {T, N} =
+    @view views.array[maybe_tuple(views.locations[index...])...]
+
+@propagate_inbounds Base.setindex!(views::Views, replacement, index::Vararg{Int, N}) where {T, N} =
+    views.array[maybe_tuple(views.locations[index...])...] = replacement
 
 struct JulienneIndexer{T, N, IS, ID} <: AbstractArray{T, N}
     indexes::IS
     indexed::ID
 end
 
-indices(j::JulienneIndexer) = getindex_unrolled(j.indexes, j.indexed)
-size(j::JulienneIndexer) = length.(indices(j))
+axes(j::JulienneIndexer) = getindex_unrolled(j.indexes, j.indexed)
+length(j::JulienneIndexer) = prod(length.(axes(j)))
+
 getindex(j::JulienneIndexer{T, N}, index::Vararg{Int, N}) where {T, N} =
-    setindex_many_unrolled(j.indexes, index, j.indexed)
+    setindex_unrolled(j.indexes, index, j.indexed)
 
 JulienneIndexer(indexes, indexed) =
     JulienneIndexer{
         typeof(map(indexed, indexes) do switch, index
-            ifelse(switch, 1, index)
+            if Bool(switch)
+                1
+            else
+                index
+            end
         end),
         length(getindex_unrolled(indexes, indexed)),
         typeof(indexes),
@@ -41,16 +68,14 @@ is_indexed(::typeof(:)) = False()
 
 export julienne
 """
-    julienne([T = Views], array, code)
+    julienne(array, code)
 
-Slice an array and create `Parts` of type `T`. `T` should be one of `Arrays`,
-`Swaps`, or `Views`. It defaults to `Views`. See the `Parts` package for more
-information. The code should a tuple of length `ndims(array)`, where `:`
-indicates an axis parallel to slices and `*` indices an axis perpendicular to
-slices.
+Slice an array and create views. The code should a tuple of length
+`ndims(array)`, where `:` indicates an axis parallel to slices and `*` axes an
+axis perpendicular to slices.
 
 ```jldoctest
-julia> using JuliennedArrays, Base.Test
+julia> using JuliennedArrays
 
 julia> array = [5 6 4; 1 3 2; 7 9 8]
 3×3 Array{Int64,2}:
@@ -58,17 +83,15 @@ julia> array = [5 6 4; 1 3 2; 7 9 8]
  1  3  2
  7  9  8
 
-julia> @inferred map(sum, julienne(array, (*, :)))
+julia> map(sum, julienne(array, (*, :)))
 3-element Array{Int64,1}:
  15
   6
  24
 ```
 """
-julienne(T, array, code) =
-    T(array, JulienneIndexer(indices(array), is_indexed.(code)))
-
-julienne(array, code) = julienne(Views, array, code)
+julienne(array, code) =
+    Views(array, JulienneIndexer(axes(array), is_indexed.(code)))
 
 export align
 """
@@ -80,46 +103,35 @@ dimensions coded by `:`, while `*` indicates dimensions taken up by the
 container array. Each slice should be EXACTLY the same size.
 
 ```jldoctest
-julia> using JuliennedArrays, MappedArrays, Base.Test
+julia> using JuliennedArrays, MappedArrays
 
 julia> code = (*, :);
 
-julia> array = [5 6 4; 1 3 2; 7 9 8]
-3×3 Array{Int64,2}:
+julia> array = [5 6 4; 1 3 2]
+2×3 Array{Int64,2}:
  5  6  4
  1  3  2
- 7  9  8
 
-julia> views = @inferred julienne(array, code);
+julia> views = julienne(array, code);
 
 julia> align(mappedarray(sort, views), code)
-3×3 Array{Int64,2}:
+2×3 Array{Int64,2}:
  4  5  6
  1  2  3
- 7  8  9
 ```
 """
 function align(input_slices::AbstractArray{<: AbstractArray}, code)
     indexed = is_indexed.(code)
-
     first_input_slice = first(input_slices)
-
     trivial = Base.OneTo(1)
 
     output_indexes =
-        setindex_many_unrolled(
-            setindex_many_unrolled(
-                indexed,
-                indices(input_slices),
-                indexed,
-                trivial),
-            indices(first_input_slice),
-            not.(indexed),
-            trivial
-        )
+        fill_tuple(indexed, trivial) |>
+        x -> setindex_unrolled(x, axes(input_slices), indexed) |>
+        x -> setindex_unrolled(x, axes(first_input_slice), not.(indexed))
 
     output = similar(first_input_slice, output_indexes...)
-    output_slices = julienne(Arrays, output, code)
+    output_slices = julienne(output, code)
 
     output_slices[1] = first_input_slice
     for i in Iterators.Drop(eachindex(input_slices), 1)
@@ -128,16 +140,14 @@ function align(input_slices::AbstractArray{<: AbstractArray}, code)
     output
 end
 
-abstract type FunctionOptimization{F} end
-
-export Reduction
+export Reduce
 """
-    struct Reduction{F}
+    struct Reduce{F}
 
-A reduction of another function. Enables optimizations in some cases.
+Reduction of another function. Enables optimizations in some cases.
 
 ```jldoctest
-julia> using JuliennedArrays, Base.Test, Base.Test
+julia> using JuliennedArrays
 
 julia> array = [5 6 4; 1 3 2; 7 9 8]
 3×3 Array{Int64,2}:
@@ -145,14 +155,14 @@ julia> array = [5 6 4; 1 3 2; 7 9 8]
  1  3  2
  7  9  8
 
-julia> @inferred map(Reduction(+), julienne(array, (*, :)))
+julia> map(Reduce(+), julienne(array, (*, :)))
 3×1 Array{Int64,2}:
  15
   6
  24
 
 julia> array = reshape(1:8, 2, 2, 2)
-2×2×2 Base.ReshapedArray{Int64,3,UnitRange{Int64},Tuple{}}:
+2×2×2 reshape(::UnitRange{Int64}, 2, 2, 2) with eltype Int64:
 [:, :, 1] =
  1  3
  2  4
@@ -161,17 +171,22 @@ julia> array = reshape(1:8, 2, 2, 2)
  5  7
  6  8
 
-julia> @inferred map(Reduction(+), julienne(array, (:, *, :)))
+julia> map(Reduce(+), julienne(array, (:, *, :)))
 1×2×1 Array{Int64,3}:
 [:, :, 1] =
  14  22
 ```
 """
-struct Reduction{F} <: FunctionOptimization{F}; f::F; end
+struct Reduce{F}
+    f::F
+end
 
-const JuliennedArray = AbstractParts{T, N, A, I} where {T, N, A, I <: JulienneIndexer}
+(r::Reduce)(x) = r.f(x)
 
-map(r::Reduction, s::JuliennedArray) =
-    mapreducedim(identity, r.f, s.array, colon_dimensions(s.locations))
+export JuliennedArray
+const JuliennedArray = Views{T, N, A, I} where {T, N, A, I <: JulienneIndexer}
+
+map(r::Reduce, s::JuliennedArray) =
+    mapreduce(identity, r.f, s.array, dims = colon_dimensions(s.locations))
 
 end
