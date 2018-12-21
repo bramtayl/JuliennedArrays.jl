c87060ba513860310d2a034c822f0aac7a1cb90e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-21 03:53:42 +0000

    build based on 4bdc186

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,14 +9,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#JuliennedArrays.Reduce",
-    "page": "JuliennedArrays.jl",
-    "title": "JuliennedArrays.Reduce",
-    "category": "type",
-    "text": "struct Reduce{F}\n\nReduction of another function. Enables optimizations in some cases.\n\njulia> using JuliennedArrays\n\njulia> array = [5 6 4; 1 3 2; 7 9 8]\n3×3 Array{Int64,2}:\n 5  6  4\n 1  3  2\n 7  9  8\n\njulia> map(Reduce(+), julienne(array, (*, :)))\n3×1 Array{Int64,2}:\n 15\n  6\n 24\n\njulia> array = reshape(1:8, 2, 2, 2)\n2×2×2 reshape(::UnitRange{Int64}, 2, 2, 2) with eltype Int64:\n[:, :, 1] =\n 1  3\n 2  4\n\n[:, :, 2] =\n 5  7\n 6  8\n\njulia> map(Reduce(+), julienne(array, (:, *, :)))\n1×2×1 Array{Int64,3}:\n[:, :, 1] =\n 14  22\n\n\n\n\n\n"
-},
-
-{
     "location": "#Base.Iterators.flatten",
     "page": "JuliennedArrays.jl",
     "title": "Base.Iterators.flatten",
