c60275d225144241ef72de72efed437c0431d5c0
Brandon Taylor <brandon.taylor221@gmail.com>
2018-12-20 20:51:36 -0500

    help

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,11 @@
-using Documenter: deploydocs
+using Documenter: makedocs, deploydocs
+import JuliennedArrays
+
+makedocs(
+    modules = [JuliennedArrays],
+    sitename = "JuliennedArrays.jl",
+    strict = true
+)
 
 deploydocs(
     repo = "github.com/bramtayl/JuliennedArrays.jl.git",
