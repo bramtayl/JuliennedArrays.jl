ede3a9fc9b25a9600ddc99b9b64d406daa833d1b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-08 16:01:40 +0000

    build based on a48703b

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,11 +9,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#JuliennedArrays.Align-Union{Tuple{M}, Tuple{N}, Tuple{T}, Tuple{AbstractArray{#s12,M} where #s12<:AbstractArray{T,N},Vararg{Any,N} where N}} where M where N where T",
+    "location": "#JuliennedArrays.True",
     "page": "JuliennedArrays.jl",
-    "title": "JuliennedArrays.Align",
-    "category": "method",
-    "text": "Align(it, along...)\n\njulia> using JuliennedArrays\n\njulia> array = [[1, 2], [3, 4]];\n\njulia> aligned = Align(array, False(), True())\n2×2 Align{Int64,2,Array{Array{Int64,1},1},Tuple{False,True}}:\n 1  2\n 3  4\n\njulia> aligned[1, 1] = 5;\n\njulia> collect(aligned)\n2×2 Array{Int64,2}:\n 5  2\n 3  4\n\n\n\n\n\n"
+    "title": "JuliennedArrays.True",
+    "category": "type",
+    "text": "struct True\n\n\n\n\n\n"
 },
 
 {
@@ -25,19 +25,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#JuliennedArrays.Slices-Tuple{Any,Vararg{Any,N} where N}",
+    "location": "#JuliennedArrays.Slices",
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.Slices",
-    "category": "method",
-    "text": "Slices(array, code...)\n\njulia> using JuliennedArrays\n\njulia> it = [1 2; 3 4];\n\njulia> Slices(it, False(), True())\n2-element Slices{SubArray{Int64,1,Array{Int64,2},Tuple{Int64,Base.OneTo{Int64}},true},1,Array{Int64,2},Tuple{False,True}}:\n [1, 2]\n [3, 4]\n\n\n\n\n\n"
+    "category": "type",
+    "text": "Slices(array, code...)\n\nSlice array into views. code shows which dimensions will be replaced with : when slicing.\n\njulia> using JuliennedArrays\n\njulia> it = [1 2; 3 4];\n\njulia> slices = Slices(it, False(), True())\n2-element Slices{SubArray{Int64,1,Array{Int64,2},Tuple{Int64,Base.OneTo{Int64}},true},1,Array{Int64,2},Tuple{False,True}}:\n [1, 2]\n [3, 4]\n\njulia> slices[1] == it[1, :]\ntrue\n\n\n\n\n\n"
 },
 
 {
-    "location": "#JuliennedArrays.True",
+    "location": "#JuliennedArrays.Align",
     "page": "JuliennedArrays.jl",
-    "title": "JuliennedArrays.True",
+    "title": "JuliennedArrays.Align",
     "category": "type",
-    "text": "struct True\n\n\n\n\n\n"
+    "text": "Align(it, along...)\n\nAlign an array of arrays, all with the same size. along shows which dimensions will be taken up by the inner arrays. Inverse of Slices.\n\njulia> using JuliennedArrays\n\njulia> array = [[1, 2], [3, 4]];\n\njulia> aligned = Align(array, False(), True())\n2×2 Align{Int64,2,Array{Array{Int64,1},1},Tuple{False,True}}:\n 1  2\n 3  4\n\njulia> aligned[1, :] == array[1]\ntrue\n\n\n\n\n\n"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.jl",
     "category": "section",
-    "text": "Modules = [JuliennedArrays]"
+    "text": "True\nFalse\nSlices\nAlign"
 },
 
 ]}
