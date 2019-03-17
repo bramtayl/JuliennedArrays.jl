381741ba647e153706302100c27ce1c77643e06e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-17 15:47:39 +0000

    build based on a8f9bec

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.True",
     "category": "type",
-    "text": "struct True\n\nA placeholder for having a : in the along a particular axis. See Slices, Align.\n\n\n\n\n\n"
+    "text": "struct True\n\nTyped true\n\n\n\n\n\n"
 },
 
 {
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.False",
     "category": "type",
-    "text": "struct False\n\nA placeholder for using the given index (as opposed to a :) along a particular axis. See Slices, Align.\n\n\n\n\n\n"
+    "text": "struct False\n\nTyped false\n\n\n\n\n\n"
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.Slices",
     "category": "type",
-    "text": "Slices(array, along...)\n\nSlice array into views. along, composed of True and False objects, shows which dimensions will be replaced with : when slicing.\n\njulia> using JuliennedArrays\n\njulia> it = [1 2; 3 4];\n\njulia> slices = Slices(it, False(), True())\n2-element Slices{SubArray{Int64,1,Array{Int64,2},Tuple{Int64,Base.OneTo{Int64}},true},1,Array{Int64,2},Tuple{False,True}}:\n [1, 2]\n [3, 4]\n\njulia> slices[1] == it[1, :]\ntrue\n\n\n\n\n\n"
+    "text": "Slices(array, along...)\n\nSlice array into views.\n\nalong, made of True and False objects, shows which dimensions will be replaced with : when slicing.\n\njulia> using JuliennedArrays\n\njulia> it = [1 2; 3 4];\n\njulia> slices = Slices(it, False(), True())\n2-element Slices{SubArray{Int64,1,Array{Int64,2},Tuple{Int64,Base.OneTo{Int64}},true},1,Array{Int64,2},Tuple{False,True}}:\n [1, 2]\n [3, 4]\n\njulia> slices[1] == it[1, :]\ntrue\n\njulia> slices[1] = [2, 1];\n\njulia> it\n2×2 Array{Int64,2}:\n 2  1\n 3  4\n\n\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "JuliennedArrays.jl",
     "title": "JuliennedArrays.Align",
     "category": "type",
-    "text": "Align(it, along...)\n\nAlign an array of arrays, all with the same size. along, composed of True and False objects, shows which dimensions will be taken up by the inner arrays. Inverse of Slices.\n\njulia> using JuliennedArrays\n\njulia> array = [[1, 2], [3, 4]];\n\njulia> aligned = Align(array, False(), True())\n2×2 Align{Int64,2,Array{Array{Int64,1},1},Tuple{False,True}}:\n 1  2\n 3  4\n\njulia> aligned[1, :] == array[1]\ntrue\n\n\n\n\n\n"
+    "text": "Align(it, along...)\n\nAlign an array of arrays, all with the same size.\n\nalong, made of True and False objects, shows which dimensions will be taken up by the inner arrays. Inverse of Slices.\n\njulia> using JuliennedArrays\n\njulia> array = [[1, 2], [3, 4]];\n\njulia> aligned = Align(array, False(), True())\n2×2 Align{Int64,2,Array{Array{Int64,1},1},Tuple{False,True}}:\n 1  2\n 3  4\n\njulia> aligned[1, :] == array[1]\ntrue\n\njulia> aligned[1, 1] = 0;\n\njulia> array\n2-element Array{Array{Int64,1},1}:\n [0, 2]\n [3, 4]\n\n\n\n\n\n"
 },
 
 {
