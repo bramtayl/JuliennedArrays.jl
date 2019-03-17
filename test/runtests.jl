26ec31e9fd6e6116e45b429e7d22dcaafeb29641
Brandon Taylor <brandon.taylor221@gmail.com>
2019-03-17 13:31:19 -0400

    bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ makedocs(sitename = "JuliennedArrays.jl", strict = true)
 
 if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" && get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
     deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
-    joinpath(@__DIR__, "..", "..", "src") |> process_folder |> submit
+    joinpath(@__DIR__, "..", "src") |> process_folder |> submit
 end
