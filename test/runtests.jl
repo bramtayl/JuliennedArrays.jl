224c1d461ca0744b52761d16f7f5ec684ac3d7cb
Tamas K. Papp <tkpapp@gmail.com>
2019-03-17 07:43:58 +0100

    build the docs in the test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,12 @@
 using JuliennedArrays
+
+# Build the docs on Julia v1.1
+if get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
+    cd(joinpath(@__DIR__, "..")) do
+        withenv("JULIA_LOAD_PATH" => nothing) do
+            cmd = `$(Base.julia_cmd()) --depwarn=no --color=yes --project=docs/`
+            run(`$(cmd) -e 'using Pkg; Pkg.instantiate()'`)
+            run(`$(cmd) docs/make.jl`)
+        end
+    end
+end
