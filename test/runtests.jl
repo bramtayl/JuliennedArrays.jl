17ec0ed55c08d92e122cffa7dc205eb9e110a317
bramtayl <brandon.taylor221@gmail.com>
2019-05-27 18:08:45 -0400

    docs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,13 @@
 using JuliennedArrays
 using Documenter: makedocs, deploydocs
 
-makedocs(sitename = "JuliennedArrays.jl", strict = true)
+makedocs(
+    sitename = "JuliennedArrays.jl",
+    strict = true,
+    modules = [JuliennedArrays]
+)
 
-if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" && get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
+if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" &&
+    get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
     deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
 end
