8569976a0b2998e06ca642cb75f5c8d4e0331f64
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-01 16:30:33 -0500

    fix?

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -2,11 +2,8 @@ using Pkg: develop, instantiate, PackageSpec
 develop(PackageSpec(path=pwd()))
 instantiate()
 
-@static if VERSION == v"1.3"
+using JuliennedArrays
+using Documenter: deploydocs, makedocs
 
-    using JuliennedArrays
-    using Documenter: deploydocs, makedocs
-
-    makedocs(sitename = "JuliennedArrays.jl", modules = [JuliennedArrays], doctest = false)
-    deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
-end
+makedocs(sitename = "JuliennedArrays.jl", modules = [JuliennedArrays], doctest = false)
+deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
