21d7cecc79b98d824e41bc104cc39e458b8b149c
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-01 16:33:07 -0500

    fix?

--- coverage/submit.jl
+++ coverage/submit.jl
@@ -1,9 +1,6 @@
-using Pkg: develop, instantiate, PackageSpec
-develop(PackageSpec(path=pwd()))
 instantiate()
 
-using JuliennedArrays
 using Coverage.Codecov: submit
 using Coverage: process_folder
 
-JuliennedArrays |> pathof |> dirname |> dirname |> process_folder |> submit
+submit(process_folder(pwd()))
