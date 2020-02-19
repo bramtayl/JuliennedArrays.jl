96903e030225cc620267941e319a4e0a9dbf20f1
Brandon Taylor <brandon.taylor221@gmail.com>
2020-02-18 22:20:34 -0500

    fix

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,9 @@
 using JuliennedArrays
 using Documenter: deploydocs, makedocs
 
-makedocs(sitename = "JuliennedArrays.jl", modules = [JuliennedArrays], doctest = false)
+makedocs(
+    sitename = "JuliennedArrays.jl",
+    modules = [JuliennedArrays],
+    doctest = false
+)
 deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
