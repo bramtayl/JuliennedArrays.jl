f756d68cbc5ce78e060fdd92f6fc7d9c032ff2b5
Johnny Chen <johnnychen94@hotmail.com>
2021-08-25 00:20:03 +0800

    move doctest to document CI
    add JuliennedArrays to docs/Project.toml, ven though we must do
    `pkg> dev .` to use local development version of it.

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 using JuliennedArrays
 using Documenter: deploydocs, makedocs
 
-makedocs(sitename = "JuliennedArrays.jl", modules = [JuliennedArrays], doctest = false)
+makedocs(sitename = "JuliennedArrays.jl", modules = [JuliennedArrays], doctest = true, strict=true)
 deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
