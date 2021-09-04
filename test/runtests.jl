c8662ff1ff3cf73695a35bf927e699810e3bc715
Brandon Taylor <brandon.taylor221@gmail.com>
2021-09-04 09:29:13 -0400

    versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using JuliennedArrays
 using Test
 using Documenter: doctest
 
-doctest(JuliennedArrays)
+if VERSION >= v"1.6"
+    doctest(JuliennedArrays)
+end
 
 @testset "JuliennedArrays.jl" begin
     @testset "Align" begin
@@ -18,6 +20,11 @@ doctest(JuliennedArrays)
         @test size(X) == (2, 1, 3, 4)
         @test permutedims(X, (1, 3, 2, 4))[:] == cat(Xs...; dims = 3)[:]
 
+        # type is inferrable for constant integer alongs
+        align_1_3(x) = Align(x, 1, 3)
+        @inferred align_1_3(Xs)
+
+        @test_throws DimensionMismatch Align([rand(2, 3) for _ in 1:4], 1)
         @test_throws MethodError Align(ones(2, 3, 4), 1, 2, 3)
     end
 
@@ -39,5 +46,14 @@ doctest(JuliennedArrays)
         @test Align(Xs, 1, 3) == X # Slices is the inverse of Align
         @test Xs[1] == X[:, 1, :, 1] # test linear indexing
         @test Xs[1, 3] == X[:, 1, :, 3] # test cartesian indexing
+
+        # type is inferrable for constant integer alongs
+        slices_1_3(x) = Slices(x, 1, 3)
+        @inferred slices_1_3(X)
+
+        X = rand(2, 3, 4, 5)
+        @test_throws MethodError Slices(X, True())
+        @test_throws MethodError Slices(X, True(), False(), False(), False(), False())
+        @test_throws ArgumentError("All alongs values (5,) should be less than or equal to 4") Slices(X, 5)
     end
 end
