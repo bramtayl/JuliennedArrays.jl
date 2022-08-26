6a749d978a2189c423f35c6b6c9d00a98d250c29
bramtayl <brandon.taylor221@gmail.com>
2022-08-25 20:59:12 -0400

    align empty arrays (#33)
    * align empty arrays
    
    * disable nightly
    
    * 1.6
    
    * coverage

--- src/JuliennedArrays.jl
+++ src/JuliennedArrays.jl
@@ -6,27 +6,6 @@ using Base: @inline, @propagate_inbounds, @pure, tail
 export Slices, Align
 export True, False
 
-function is_in(::Needle, ::Needle, _...) where {Needle}
-    True()
-end
-function is_in(needle, _, straws...)
-    is_in(needle, straws...)
-end
-function is_in(_)
-    False()
-end
-
-function in_unrolled(straws, needle1, needles...)
-    is_in(needle1, straws...), in_unrolled(straws, needles...)...
-end
-function in_unrolled(_)
-    ()
-end
-
-@pure function as_vals(them::Int...)
-    map(Val, them)
-end
-
 abstract type TypedBool end
 """
     struct True
@@ -78,15 +57,55 @@ function setindex_unrolled(old, new, switches)
     end
 end
 
+function find_in_error(::Val{along}) where {along}
+    throw(ArgumentError("$along, a dimension number, is out of bounds or out of order."))
+end
+
+function find_in_skip(alongs, dimensions)
+    (False(), find_in(alongs, tail(dimensions))...)
+end
+
+function find_in_check(
+    first_along::Val{along},
+    ::Val{dimension},
+    alongs,
+    dimensions,
+) where {along, dimension}
+    if dimension < along
+        find_in_skip(alongs, dimensions)
+    elseif dimension == along
+        (True(), find_in(tail(alongs), tail(dimensions))...)
+    else
+        # dimension > along
+        find_in_error(first_along)
+    end
+end
+
+function find_in(::Tuple{}, ::Tuple{})
+    ()
+end
+function find_in(alongs::Tuple{}, dimensions)
+    find_in_skip(alongs, dimensions)
+end
+function find_in(alongs, ::Tuple{})
+    find_in_error(first(alongs))
+end
+function find_in(alongs, dimensions)
+    find_in_check(first(alongs), first(dimensions), alongs, dimensions)
+end
+
 ###
 # Slices
 ###
-struct Slices{Item,Dimensions,Whole,Alongs} <: AbstractArray{Item,Dimensions}
+struct Slices{Item, Dimensions, Whole, Alongs} <: AbstractArray{Item, Dimensions}
     whole::Whole
     alongs::Alongs
 end
-function Slices{Item,Dimensions}(whole::Whole, alongs::Alongs) where {Item,Dimensions,Whole,Alongs}
-    Slices{Item,Dimensions,Whole,Alongs}(whole, alongs)
+function Slices{Item, Dimensions}(
+    whole::Whole,
+    alongs::Alongs,
+) where {Item, Dimensions, Whole, Alongs}
+    Slices{Item, Dimensions, Whole, Alongs}(whole, alongs)
 end
 
 function axes(slices::Slices)
@@ -100,20 +119,20 @@ function slice_index(slices, indices)
     setindex_unrolled(axes(slices.whole), indices, map(not, slices.alongs))
 end
 @inline @propagate_inbounds function getindex(
-    slices::Slices{Item,Dimensions},
-    indices::Vararg{Int,Dimensions},
-) where {Item,Dimensions}
+    slices::Slices{Item, Dimensions},
+    indices::Vararg{Int, Dimensions},
+) where {Item, Dimensions}
     view(slices.whole, slice_index(slices, indices)...)
 end
 @inline @propagate_inbounds function setindex!(
-    slices::Slices{Item,Dimensions},
+    slices::Slices{Item, Dimensions},
     value,
-    indices::Vararg{Int,Dimensions},
-) where {Item,Dimensions}
+    indices::Vararg{Int, Dimensions},
+) where {Item, Dimensions}
     slices.whole[slice_index(slices, indices)...] = value
 end
 
-function axis_or_1(switch, axis)
+@inline function axis_or_1(switch, axis)
     if untyped(switch)
         axis
     else
@@ -165,8 +184,8 @@ julia> @test_throws MethodError Slices(whole, True());
 ```
 """
 function Slices(
-    whole::AbstractArray{<:Any,Dimensions},
-    alongs::Vararg{TypedBool,Dimensions},
+    whole::AbstractArray{<:Any, Dimensions},
+    alongs::Vararg{TypedBool, Dimensions},
 ) where {Dimensions}
     Slices{
         typeof(@inbounds view(whole, map(axis_or_1, alongs, axes(whole))...)),
@@ -207,10 +226,11 @@ julia> map(sum, s)
 ```
 
 No along should be greater than the number of dimensions of `whole`.
+`alongs` should also be strictly increasing.
 
 ```jldoctest slices_ints
 julia> Slices(input, 4)
-ERROR: ArgumentError: All alongs values (4,) should be less than or equal to 3
+ERROR: ArgumentError: 4, a dimension number, is out of bounds or out of order.
 [...]
 ```
 
@@ -228,22 +248,13 @@ julia> @inferred slices_1_3(input)
 ```
 """
 @inline function Slices(
-    whole::AbstractArray{Item,Dimensions},
+    whole::AbstractArray{Item, Dimensions},
     alongs::Int...,
-) where {Item,Dimensions}
-    value_alongs = as_vals(alongs...)
-    value_dimensions = ntuple(Val, Dimensions)
-    if untyped(is_in(False(), in_unrolled(value_dimensions, value_alongs...)...))
-        throw(
-            ArgumentError(
-                "All alongs values $(alongs) should be less than or equal to $(Dimensions)",
-            ),
-        )
-    end
-    Slices(whole, in_unrolled(value_alongs, value_dimensions...)...)
+) where {Item, Dimensions}
+    Slices(whole, find_in(map(Val, alongs), ntuple(Val, Dimensions))...)
 end
 
-function Base.showarg(io::IO, ::Slices{Item,Dimensions}, toplevel) where {Item,Dimensions}
+function Base.showarg(io::IO, ::Slices{Item, Dimensions}, toplevel) where {Item, Dimensions}
     print(
         io,
         "Slices{",
@@ -267,67 +278,27 @@ function basetype(Item)
     basetype(typeof(Item))
 end
 
-@inline function Int(::True)
-    1
-end
-@inline function Int(::False)
-    0
-end
-
-function match_dimensions(has_dimensions, used_dimensions::Int)
-    if used_dimensions !== has_dimensions
-        throw(
-            DimensionMismatch(
-                "$used_dimensions of $has_dimensions inner dimensions are used",
-            ),
-        )
-    end
-end
-
-function check_dimensions(has_dimensions, ::Vararg{Int,Dimensions}) where {Dimensions}
-    match_dimensions(has_dimensions, Dimensions)
-end
-
-function check_dimensions(has_dimensions, alongs::Vararg{TypedBool})
-    match_dimensions(has_dimensions, mapreduce(Int, +, alongs))
-end
-
-function check_sizes(slices, alongs...)
-    if !isempty(slices)
-        first_slice = first(slices)
-        first_axes = axes(first_slice)
-        check_dimensions(ndims(first_slice), alongs...)
-        for slice in slices
-            if axes(slice) != first_axes
-                throw(
-                    ArgumentError(
-                        "Axes of $slice does not match the axes of the first slice: $first_axes",
-                    ),
-                )
-            end
-        end
-    end
-end
-
 ###
 # Align
 ###
-struct Align{Item,Dimensions,Sliced,Alongs} <: AbstractArray{Item,Dimensions}
+struct Align{Item, Dimensions, Sliced, Alongs, SliceAxes} <: AbstractArray{Item, Dimensions}
     slices::Sliced
     alongs::Alongs
+    slice_axes::SliceAxes
 end
 
-function Align{Item,Dimensions}(
+function Align{Item, Dimensions}(
     slices::Sliced,
-    alongs::NTuple{Dimensions,TypedBool},
-) where {Item,Dimensions,Sliced}
-    Align{Item,Dimensions,Sliced,typeof(alongs)}(slices, alongs)
+    alongs::NTuple{Dimensions, TypedBool},
+    slice_axes::SliceAxes,
+) where {Item, Dimensions, Sliced, SliceAxes}
+    Align{Item, Dimensions, Sliced, typeof(alongs), SliceAxes}(slices, alongs, slice_axes)
 end
 
 function axes(aligned::Align)
     setindex_unrolled(
         setindex_unrolled(aligned.alongs, axes(aligned.slices), map(not, aligned.alongs)),
-        axes(first(aligned.slices)),
+        aligned.slice_axes,
         aligned.alongs,
     )
 end
@@ -340,27 +311,28 @@ function split_indices(aligned, indices)
     getindex_unrolled(indices, aligned.alongs)
 end
 @inline @propagate_inbounds function getindex(
-    aligned::Align{Item,Dimensions},
-    indices::Vararg{Int,Dimensions},
-) where {Item,Dimensions}
+    aligned::Align{Item, Dimensions},
+    indices::Vararg{Int, Dimensions},
+) where {Item, Dimensions}
     outer, inner = split_indices(aligned, indices)
     aligned.slices[outer...][inner...]
 end
 @inline @propagate_inbounds function setindex!(
-    aligned::Align{Item,Dimensions},
+    aligned::Align{Item, Dimensions},
     value,
-    indices::Vararg{Int,Dimensions},
-) where {Item,Dimensions}
+    indices::Vararg{Int, Dimensions},
+) where {Item, Dimensions}
     outer, inner = split_indices(aligned, indices)
     aligned.slices[outer...][inner...] = value
 end
 
 """
-    Align(slices, alongs::TypedBool...)
+    Align(slices, alongs::TypedBool...; slice_axes = axes(first(slices)))
 
 `Align` an array of arrays, all with the same size.
 
-`alongs`, made of [`True`](@ref) and [`False`](@ref) objects, shows which dimensions will be taken up by the inner arrays. Inverse of [`Slices`](@ref).
+`alongs`, made of [`True`](@ref) and [`False`](@ref) objects, shows which dimensions will be taken up by the inner arrays.
+Inverse of [`Slices`](@ref).
 
 ```jldoctest align_bools
 julia> using JuliennedArrays
@@ -390,16 +362,33 @@ Use `@inbounds` to skip this check.
 julia> unequal = [[1], [1, 2]];
 
 julia> Align(unequal, False(), True())
-ERROR: ArgumentError: Axes of [1, 2] does not match the axes of the first slice: (Base.OneTo(1),)
+ERROR: ArgumentError: Slice [1, 2] does not have slice_axes (Base.OneTo(1),)
 [...]
+```
+
+`slice_axes` is the axes of one slice.
+If `slices` is empty, you can specify `slice_axes` manually.
+
+```jldoctest align_bools
+julia> Align([rand(3) for _ in 1:0], True(), False())
+ERROR: BoundsError: attempt to access 0-element Vector{Vector{Float64}} at index [1]
+[...]
+
+julia> Align([rand(3) for _ in 1:0], True(), False(); slice_axes = axes(rand(3)))
+3×0 Align{Float64, 2} with eltype Float64
 ```
 """
 @inline @propagate_inbounds function Align(
-    slices::AbstractArray{<:AbstractArray{Item,InnerDimensions},OuterDimensions},
-    alongs::TypedBool...,
-) where {Item,InnerDimensions,OuterDimensions}
-    @boundscheck check_sizes(slices, alongs...)
-    Align{Item,OuterDimensions + InnerDimensions}(slices, alongs)
+    slices::AbstractArray{<:AbstractArray{Item, InnerDimensions}, OuterDimensions},
+    alongs::TypedBool...;
+    slice_axes = axes(first(slices)),
+) where {Item, InnerDimensions, OuterDimensions}
+    @boundscheck for slice in slices
+        if axes(slice) != slice_axes
+            throw(ArgumentError("Slice $slice does not have slice_axes $slice_axes."))
+        end
+    end
+    Align{Item, OuterDimensions + InnerDimensions}(slices, alongs, slice_axes)
 end
 
 """
@@ -437,10 +426,11 @@ julia> Align(slices, 1, 3)
 ```
 
 You must include one `along` for each inner dimension.
+`alongs` should also be strictly increasing.
 
 ```jldoctest align_ints
 julia> Align(slices, 1)
-ERROR: DimensionMismatch("1 of 2 inner dimensions are used")
+ERROR: ArgumentError: (1,) is not of length inner dimensions (2).
 [...]
 ```
 
@@ -461,22 +451,37 @@ julia> @inferred align_1_3(slices)
  5  7
  6  8 
 ```
+
+`slice_axes` is the axes of one slice.
+If `slices` is empty, you can specify `slice_axes` manually.
+
+```jldoctest align_ints
+julia> Align([rand(3) for _ in 1:0], 1)
+ERROR: BoundsError: attempt to access 0-element Vector{Vector{Float64}} at index [1]
+[...]
+
+julia> Align([rand(3) for _ in 1:0], 1; slice_axes = axes(rand(3)))
+3×0 Align{Float64, 2} with eltype Float64
+```
 """
 @inline @propagate_inbounds function Align(
-    slices::AbstractArray{<:AbstractArray{Item,InnerDimensions},OuterDimensions},
-    alongs::Int...,
-) where {Item,InnerDimensions,OuterDimensions}
-    @boundscheck check_sizes(slices, alongs...)
+    slices::AbstractArray{<:AbstractArray{Item, InnerDimensions}, OuterDimensions},
+    alongs::Int...;
+    slice_axes = axes(first(slices)),
+) where {Item, InnerDimensions, OuterDimensions}
+    if length(alongs) != InnerDimensions
+        throw(
+            ArgumentError("$alongs is not of length inner dimensions ($InnerDimensions)."),
+        )
+    end
     Align(
         slices,
-        in_unrolled(
-            as_vals(alongs...),
-            ntuple(Val, InnerDimensions + OuterDimensions)...,
-        )...,
+        find_in(map(Val, alongs), ntuple(Val, InnerDimensions + OuterDimensions))...;
+        slice_axes = slice_axes,
     )
 end
 
-function Base.showarg(io::IO, ::Align{Item,Dimensions}, toplevel) where {Item,Dimensions}
+function Base.showarg(io::IO, ::Align{Item, Dimensions}, toplevel) where {Item, Dimensions}
     print(io, "Align{", Item, ", ", Dimensions, "}")
     toplevel && print(io, " with eltype ", Item)
 end
